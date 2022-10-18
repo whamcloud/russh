7b12f662f647b6593fa55afa676e7fd84060ccf8
Joe Grund <jgrund@whamcloud.io>
2022-10-18 16:41:38 -0400

    Implement detached signal sender
    Allow signals to be sent detached from the channel struct.
    
    This is done using a higher-order function and by cloning the sender so
    it can be used independently of the `Channel`.
    
    This is useful to be able to spawn a cancelation handler separately from
    the main `Channel` recieve loop.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- russh/src/channels.rs
+++ russh/src/channels.rs
@@ -1,3 +1,6 @@
+use std::pin::Pin;
+
+use futures::{Future, FutureExt};
 use russh_cryptovec::CryptoVec;
 use tokio::sync::mpsc::{Sender, UnboundedReceiver};
 
@@ -119,7 +122,7 @@ impl<T: From<(ChannelId, ChannelMsg)>> std::fmt::Debug for Channel<T> {
     }
 }
 
-impl<S: From<(ChannelId, ChannelMsg)> + Send + 'static> Channel<S> {
+impl<Send: From<(ChannelId, ChannelMsg)> + std::marker::Send + 'static> Channel<Send> {
     pub fn id(&self) -> ChannelId {
         self.id
     }
@@ -179,9 +182,31 @@ impl<S: From<(ChannelId, ChannelMsg)> + Send + 'static> Channel<S> {
     }
 
     /// Signal a remote process.
-    pub async fn signal(&mut self, signal: Sig) -> Result<(), Error> {
+    pub async fn signal(self, signal: Sig) -> Result<(), Error> {
         self.send_msg(ChannelMsg::Signal { signal }).await?;
+
         Ok(())
+    }
+
+    /// Get a `FnOnce` that can be used to send a signal through this channel
+    pub fn get_signal_sender(
+        &self,
+    ) -> impl FnOnce(Sig) -> Pin<Box<dyn Future<Output = Result<(), Error>> + std::marker::Send>>
+    {
+        let sender = self.sender.clone();
+        let id = self.id;
+
+        move |signal| {
+            async move {
+                sender
+                    .send((id, ChannelMsg::Signal { signal }).into())
+                    .await
+                    .map_err(|_| Error::SendError)?;
+
+                Ok(())
+            }
+            .boxed()
+        }
     }
 
     /// Request the start of a subsystem with the given name.
